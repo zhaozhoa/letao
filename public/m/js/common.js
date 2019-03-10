821fd232998cf4517912d567492b3357a97595e0
zhangzhao <zhangzhao314@qq.com>
2019-03-10 11:09:23 +0800

    添加了未登录状态添加商品的ajaxx 处理

--- public/m/js/common.js
+++ public/m/js/common.js
@@ -13,3 +13,28 @@ CT.getParamsByUrl = function () {
   }
   return params;
 }
+
+// 需要登陆的 ajax 请求
+CT.loginUrl = '/m/user/login.html'
+CT.cartUrl = '/m/user/cart.html'
+CT.loginAjax = function (params) {  
+  $.ajax({
+    type: params.type || get,
+    url: params.url,
+    data: params.data,
+    dataType: params.dataType || 'json',
+    success: function (data) {
+      // 未登录后台返回{error: 400, message: "未登录！"}
+      if (data.error == 400) {
+        // 跳转到登陆页，把当前地址传递到登陆页，当登陆成功再跳回来
+        location.href = `${CT.loginUrl}?returnUrl=${location.href}`
+        return false
+      } else {
+        params.success && params.success(data)
+      }
+    },
+    error: function  () {
+      mui.toast('服务器繁忙')
+    }
+  });
+}
